944ef91585b6e3f7752b9cf3d7e8276bb2c14f9c
MdAfsarHossain <afsar291722@gmail.com>
2025-05-25 14:32:13 +0600

    Insert some data into the Sightings table.

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -71,3 +71,41 @@ CREATE TABLE sightings (
     FOREIGN KEY (ranger_id) REFERENCES rangers (ranger_id),
     FOREIGN KEY (species_id) REFERENCES species (species_id)
 );
+
+-- Insert data into the Sightings table
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
