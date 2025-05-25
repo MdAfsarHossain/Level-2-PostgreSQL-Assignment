0d6cd839d8736cd15d9a72279e0de9702ad8d183
MdAfsarHossain <afsar291722@gmail.com>
2025-05-25 14:34:57 +0600

    Problem 7 Query added.

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -156,3 +156,13 @@ FROM
     JOIN rangers r ON s.ranger_id = r.ranger_id
 ORDER BY s.sighting_time DESC
 LIMIT 2;
+
+-- Problem 7
+UPDATE species
+SET
+    conservation_status = 'Historic'
+WHERE
+    EXTRACT(
+        YEAR
+        FROM discovery_date
+    ) < 1800;
